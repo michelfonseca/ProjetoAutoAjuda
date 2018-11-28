89c4c06165d22fb1116218bfac59b6443732abf3
Michel Fonseca <michel.alegret@gmail.com>
2018-11-28 17:13:34 -0200

    Correção de bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  FirebaseConfig: {
+    apiKey: "AIzaSyCY_eq6wwcISfml8z-95viZrQsesMxe4aE",
+    authDomain: "autoajudaunisep.firebaseapp.com",
+    databaseURL: "https://autoajudaunisep.firebaseio.com",
+    projectId: "autoajudaunisep",
+    storageBucket: "autoajudaunisep.appspot.com",
+    messagingSenderId: "163288648266"
+  }
 };
